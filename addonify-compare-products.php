1c5c4c1efc974854a40d1fdbbc859c9ea8f41817
Sanil Shakya <sonill_ss@hotmail.com>
2020-08-04 23:00:05 +0545

    done : settings page - with some bugs probably

--- addonify-compare-products.php
+++ addonify-compare-products.php
@@ -36,6 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'ADDONIFY_COMPARE_PRODUCTS_VERSION', '1.0.0' );
+define( 'ADDONIFY_CP_DB_INITIALS', 'addonify_cp_' );
 
 /**
  * The code that runs during plugin activation.
