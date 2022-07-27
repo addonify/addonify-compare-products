e97f708bfcd9e996b51125bb3deab2646103c7c0
h1dd3n_sn1p3r <struggleage@gmail.com>
2022-07-27 17:18:52 +0545

    Added: Button icon position style.
    Added: Button icon position style.

--- public/templates/addonify-compare-products-content.php
+++ public/templates/addonify-compare-products-content.php
@@ -23,6 +23,13 @@ defined( 'ABSPATH' ) || exit;
 
 <div id="addonify-compare-products-table-wrapper">
 
+	<button id="addonify-compare-close-button" class="addonify-cp-fake-button addonify-compare-all-close-btn">
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+			stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+			<line x1="18" y1="6" x2="6" y2="18"></line>
+			<line x1="6" y1="6" x2="18" y2="18"></line>
+		</svg>
+	</button>
 	<p id="addonify-compare-products-notice" class="<?php echo esc_attr( implode( ' ', $message_css_classes ) ); ?>">
 		<?php echo esc_html( $no_table_rows_message ); ?>
 	</p><!-- #addonify-compare-products-notice -->
