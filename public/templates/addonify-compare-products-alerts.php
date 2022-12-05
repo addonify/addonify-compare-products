203248264e35bf8a9df7b302b9ee21284f49f629
Anuj Subedi <struggleage@gmail.com>
2022-12-05 15:42:16 +0545

    :ambulance: Added: Alerts template.
    - Added: Alerts template.

---
+++ public/templates/addonify-compare-products-alerts.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+*
+* Addonify Compare Products Alerts
+* Since: 1.1.2
+*
+*/
+?>
+
+<div class="addonify-compare-alert info">
+    <div class="addonify-compare-alert-content">
+        <p></p>
+    </div><!-- // addonify-compare-alert-content -->
+</div><!-- // addonify-compare-modal-alert -->
