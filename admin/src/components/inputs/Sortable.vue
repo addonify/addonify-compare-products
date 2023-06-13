66a656d3d0795721a7b2a4a6abd883289c3ecd71
Anuj Subedi <struggleage@gmail.com>
2023-06-13 12:24:43 +0545

    Added: Notice for sortable component.
    - Added: Notice for sortable component.

--- admin/src/components/inputs/Sortable.vue
+++ admin/src/components/inputs/Sortable.vue
@@ -1,9 +1,9 @@
 <script setup>
-	import { computed, watch } from "vue";
-	import { useOptionsStore } from "../../stores/options";
+	import { computed } from "vue";
 	import draggable from "vuedraggable";
 	import { ElSwitch } from "element-plus";
 	import { Check, Close } from "@element-plus/icons-vue";
+	import Notice from "../layouts/Notice.vue";
 
 	/**
 	 *
@@ -16,6 +16,10 @@
 		modelValue: {
 			type: [String, Array, Object], // Loose type checking.
 			required: true,
+		},
+		fallbackText: {
+			type: String,
+			required: false,
 		},
 	});
 
@@ -36,85 +40,83 @@
 			emit("update:modelValue", newValue);
 		},
 	});
+
+	/**
+	 *
+	 * Import i18n.
+	 *
+	 */
+
+	const { __ } = wp.i18n;
+
+	/**
+	 *
+	 * Fall back text for fallbackText prop.
+	 *
+	 */
+
+	let fallbackText = props.fallbackText
+		? props.fallbackText
+		: __("No choices found.", "addonify-compare-products");
+
+	/**
+	 *
+	 * Return current timestamp.
+	 *
+	 * @param {void} void
+	 * @return {number} timestamp
+	 * @since 1.1.9
+	 */
+
+	const currentTime = () => new Date().getTime();
+
+	console.log(value);
 </script>
 <template>
-	<div class="adfy-draggable-elements">
-		<draggable
-			v-model="value"
-			animation="400"
-			easing="ease-in-out"
-			item-key="id"
-			direction="vertical"
-			@start="drag = true"
-			@end="drag = false"
-		>
-			<template #item="{ element }">
-				<div class="adfy-draggable-element" :key="element.id">
-					<div class="adfy-draggable-box">
-						<div class="draggable-switch">
-							<el-switch
-								v-model="element.status"
-								size="large"
-								inline-prompt
-								:active-icon="Check"
-								:inactive-icon="Close"
-							/>
-						</div>
-						<div class="label-icon-box">
-							<p class="option-label">{{ element.name }}</p>
-							<span class="option-icon">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="currentColor"
-									viewBox="0 0 24 24"
-								>
-									<path
-										d="M7 20h2V8h3L8 4 4 8h3zm13-4h-3V4h-2v12h-3l4 4z"
-									/>
-								</svg>
-							</span>
-						</div>
-					</div>
-				</div>
-			</template>
-		</draggable>
-		<!--<Sortable
-			:list="props.elements"
-			item-key="id"
-			tag="div"
-			@update="handleUpdate"
-			@change="handleChange"
-			:options="options"
-		>
-			<template #item="{ element, index }">
-				<div class="adfy-draggable-element" :key="element.id">
-					<div class="adfy-draggable-box">
-						<div class="draggable-switch">
-							<el-switch
-								v-model="valll"
-								size="large"
-								inline-prompt
-								:active-icon="Check"
-								:inactive-icon="Close"
-							/>
-						</div>
-						<div class="label-icon-box">
-							<p class="option-label">{{ element.name }}</p>
-							<span class="option-icon">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="currentColor"
-									viewBox="0 0 24 24"
-								>
-									<path
-										d="M7 20h2V8h3L8 4 4 8h3zm13-4h-3V4h-2v12h-3l4 4z"
-									/>
-								</svg>
-							</span>
+	<template v-if="value.length !== 0">
+		<div class="adfy-draggable-elements" :v-node="currentTime()">
+			<draggable
+				v-model="value"
+				animation="400"
+				easing="ease-in-out"
+				item-key="id"
+				direction="vertical"
+				@start="drag = true"
+				@end="drag = false"
+			>
+				<template #item="{ element }">
+					<div class="adfy-draggable-element" :key="element.id">
+						<div class="adfy-draggable-box">
+							<div class="draggable-switch">
+								<el-switch
+									v-model="element.status"
+									size="large"
+									inline-prompt
+									:active-icon="Check"
+									:inactive-icon="Close"
+								/>
+							</div>
+							<div class="label-icon-box">
+								<p class="option-label">{{ element.name }}</p>
+								<span class="option-icon">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="currentColor"
+										viewBox="0 0 24 24"
+									>
+										<path
+											d="M7 20h2V8h3L8 4 4 8h3zm13-4h-3V4h-2v12h-3l4 4z"
+										/>
+									</svg>
+								</span>
+							</div>
 						</div>
 					</div>
-				</div>
-			</template>
-		</Sortable>-->
-	</div>
+				</template>
+			</draggable>
+		</div>
+	</template>
+	<template v-else>
+		<Notice :content="fallbackText" type="info" />
+	</template>
 </template>
